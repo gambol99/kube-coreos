cc71bf1634dc9e7bb39b1862e0190a6e8106a0dc
Rohith <gambol99@gmail.com>
2016-06-17 10:33:42 +0100

    - fixing the unit.sh script

--- scripts/units.sh
+++ scripts/units.sh
@@ -4,6 +4,8 @@
 #
 source scripts/environment.sh || exit 1
 
+FLEETCTL="/usr/bin/fleetctl --endpoint=https://127.0.0.1:2379"
+
 destroy_unit() {
   if ${FLEETCTL} destroy ${1} >/dev/null 2>&1; then
     annonce "successfully destoryed the ${1}"
@@ -20,35 +22,45 @@ start_unit() {
   fi
 }
 
+list() {
+  ${FLEETCTL} list-units -no-legend=false -full 2>/dev/null
+}
+
+machines() {
+  ${FLEETCTL} list-machines -no-legend=false -full  2>/dev/null
+}
+
 deploy() {
   annonce "deploying the fleet units"
-  for _unit in units/*/**; do
+  for _unit in units/*; do
     unit_name=$(basename ${_unit})
     # step: check the status of the unit
-    if ${FLEETCTL} list-units -no-legend=false -full | grep "^${unit_name}.*dead"; then
+    if ${FLEETCTL} list-units -no-legend=false -full 2>/dev/null | grep "^${unit_name}.*dead"; then
       annonce "unit: ${_unit} appears to be dead, destorying and restarting the service now"
       destroy_unit ${_unit}
     fi
 
-    annonce "starting the unit service: ${_unit}"
+    annonce "checking the unit service is running: ${_unit}"
     if ${FLEETCTL} start ${_unit} 2>&1 | grep -q "^WARNING: Unit ${unit_name} in registry differs from local unit file"; then
       annonce "unit: ${unit_name} is out of sync, redeploying the unit now"
-      start_unit ${_unit}
+      destroy_unit ${_unit} && start_unit ${_unit}
     fi
   done
 }
 
 destroy() {
   annonce "deleting all the fleet units from the cluster"
-  for _unit in units/*/**; do
+  for _unit in units/*; do
     annonce "destroying the unit ${_unit}"
     unit_name=$(basename ${_unit})
-    ${FLEETCTL} destroy ${_unit} || failed "unable to destroy the unit file ${_unit}"
+    ${FLEETCTL} destroy ${_unit} 2>/dev/null || failed "unable to destroy the unit file ${_unit}"
   done
 }
 
 case "$1" in
-  destroy)    destory;  ;;
+  destroy)    destroy;  ;;
   deploy)     deploy;   ;;
+  list)       list;     ;;
+  machines)   machines; ;;
   *)          deploy;   ;;
 esac
